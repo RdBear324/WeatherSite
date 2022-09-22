5c0b943740e6b3850c574e2f8ad466ef238de406
Artem <artemka757571@gmail.com>
2022-09-22 18:53:54 +0300

    add weather app, views, created index.html

--- WeatherApp/WeatherApp/settings.py
+++ WeatherApp/WeatherApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2zj+)o91nu4)fi1!iq-d(tlv@i2=_+_+e$dc_rkry7vx*ib^62'
+SECRET_KEY = 'django-insecure-01yumr*bia4g#neo8lmsb#(vbvkeal9w^8+ml73kk(g35yf0z0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 ]
 
 MIDDLEWARE = [
