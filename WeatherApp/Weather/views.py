5a4dc0d2cd568d1263316a3ada8230bf5684cc68
Admin <test>
2022-10-25 16:45:58 +0300

    Project rebuilde

--- WeatherApp/Weather/views.py
+++ WeatherApp/Weather/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import requests
+
+API_KEY = 'f4515324282ff1f3b719932ba1abe1e6'
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + API_KEY
+    city = 'Minsk'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+    context = {
+        'info': city_info
+    }
+    return render(request, 'weather/index.html', context)
