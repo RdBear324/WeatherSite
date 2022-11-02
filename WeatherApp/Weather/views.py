e05a41c7af3d77ac5487356296a55b1b7e71cf93
Admin <test>
2022-11-02 22:04:30 +0300

    Views.py modified, index.html now show  several cities

--- WeatherApp/Weather/views.py
+++ WeatherApp/Weather/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 API_KEY = 'f4515324282ff1f3b719932ba1abe1e6'
 
 
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + API_KEY
-    city = 'Minsk'
-    res = requests.get(url.format(city)).json()
+    cities = City.objects.all()
 
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon']
-    }
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)
+
     context = {
-        'info': city_info
+        'all_info': all_cities
     }
     return render(request, 'weather/index.html', context)
